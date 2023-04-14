88e6769e147867dca8fbcb3c0e5da2051edbb61b
ASHIMA PRASAD <60879260+ashimap09@users.noreply.github.com>
2023-04-14 19:12:09 +0100

    Create globalResults.js

---
+++ globalResults.js
@@ -0,0 +1,21 @@
+[{
+      platform: "Facebook",
+      count: 548
+    },
+    {
+      platform: "Instagram",
+      count: 916
+    },
+    {
+      platform: "Snapchat",
+      count: 86
+    },
+    {
+      platform: "Twitter",
+      count: 179
+    },
+    {
+      platform: "None",
+      count: 947
+    },
+  ];
