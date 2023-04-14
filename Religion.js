798ac35451cfd29e1b9f339e189c6834fbaaded4
ASHIMA PRASAD <60879260+ashimap09@users.noreply.github.com>
2023-04-14 19:18:53 +0100

    Create Religion.js

---
+++ Religion.js
@@ -0,0 +1,34 @@
+[
+  {
+    Identity: "Christian",
+    Facebook: 21,
+    Instagram: 35,
+    Snapchat: 3,
+    Twitter: 8,
+    None: 34
+  },
+  {
+    Identity: "Jewish",
+    Facebook: 29,
+    Instagram: 29,
+    Snapchat: 4,
+    Twitter: 3,
+    None: 35
+  },
+  {
+    Identity: "Muslim",
+    Facebook: 14,
+    Instagram: 24,
+    Snapchat: 14,
+    Twitter: 10,
+    None: 38
+  },
+  {
+    Identity: "None/Other",
+    Facebook: 23,
+    Instagram: 31,
+    Snapchat: 3,
+    Twitter: 5,
+    None: 39
+  }
+];
