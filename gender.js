fdbb8af1210181d214f652f90523351aada7487a
ASHIMA PRASAD <60879260+ashimap09@users.noreply.github.com>
2023-04-14 19:25:28 +0100

    Create gender.js

---
+++ gender.js
@@ -0,0 +1,18 @@
+[
+  {
+    Gender: "female",
+    Facebook: 222,
+    Instagram: 535,
+    None: 271,
+    Snapchat: 29,
+    Twitter: 57
+  },
+  {
+    Gender: "male",
+    Facebook: 326,
+    Instagram: 380,
+    None: 678,
+    Snapchat: 56,
+    Twitter: 122
+  }
+];
