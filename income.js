bfeb44afd4e8d4b6ea4706f64296b36c7ceda20e
ASHIMA PRASAD <60879260+ashimap09@users.noreply.github.com>
2023-04-14 19:39:01 +0100

    Create income.js

---
+++ income.js
@@ -0,0 +1,32 @@
+[{
+        Identity: "Poor (< ~$50K)",
+        Facebook: 15,
+        Instagram: 45,
+        Snapchat: 7,
+        Twitter: 10,
+        None: 57
+      },
+      {
+        Identity: "Middle / lower-middle class (~$90K)",
+        Facebook: 41,
+        Instagram: 111,
+        Snapchat: 12,
+        Twitter: 32,
+        None: 144
+      },
+      {
+        Identity: "Upper-middle class (~$160K)",
+        Facebook: 71,
+        Instagram: 149,
+        Snapchat: 11,
+        Twitter: 42,
+        None: 178
+      },
+      {
+        Identity: "Upper class (> $240K)",
+        Facebook: 70,
+        Instagram: 83,
+        Snapchat: 8,
+        Twitter: 13,
+        None: 130
+      }]
