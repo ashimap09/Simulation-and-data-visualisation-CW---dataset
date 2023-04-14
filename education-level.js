8a390a696058e9f7b81373cc566b05d4b0b0be78
ASHIMA PRASAD <60879260+ashimap09@users.noreply.github.com>
2023-04-14 19:30:36 +0100

    Create education-level.js

---
+++ education-level.js
@@ -0,0 +1,44 @@
+[
+  {
+    "Education level": "High School",
+    Facebook: 29,
+    Instagram: 96,
+    Snapchat: 14,
+    Twitter: 32,
+    None: 106
+  },
+  {
+    "Education level": "College",
+    Facebook: 499,
+    Instagram: 781,
+    Snapchat: 74,
+    Twitter: 151,
+    None: 820
+  },
+  {
+    "Education level": "Grad School",
+    Facebook: 7,
+    Instagram: 13,
+    Snapchat: 4,
+    Twitter: 3,
+    None: 14
+  },
+  {
+    "Education level": "Post-grad",
+    Facebook: 15,
+    Instagram: 35,
+    Snapchat: 1,
+    Twitter: 1,
+    None: 43
+  },
+  {
+    "Education level": "Other",
+    Facebook: 3,
+    Instagram: 6,
+    Snapchat: 2,
+    Twitter: 0,
+    None: 8
+  }
+];
+
+
