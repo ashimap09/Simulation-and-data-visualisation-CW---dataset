91503250a3428885a65c6ed53ceb55c91e29a837
ASHIMA PRASAD <60879260+ashimap09@users.noreply.github.com>
2023-04-14 19:21:58 +0100

    Create race.js

---
+++ race.js
@@ -0,0 +1,50 @@
+[
+  {
+    Identity: "Asian",
+    Facebook: 27,
+    Instagram: 32,
+    Snapchat: 4,
+    Twitter: 2,
+    None: 36
+  },
+  {
+    Identity: "Black",
+    Facebook: 14,
+    Instagram: 44,
+    Snapchat: 3,
+    Twitter: 9,
+    None: 31
+  },
+  {
+    Identity: "Hispanic",
+    Facebook: 18,
+    Instagram: 34,
+    Snapchat: 3,
+    Twitter: 10,
+    None: 36
+  },
+  {
+    Identity: "Native American",
+    Facebook: 15,
+    Instagram: 46,
+    Snapchat: 0,
+    Twitter: 15,
+    None: 23
+  },
+  {
+    Identity: "White",
+    Facebook: 22,
+    Instagram: 32,
+    Snapchat: 3,
+    Twitter: 6,
+    None: 36
+  },
+  {
+    Identity: "Other",
+    Facebook: 22,
+    Instagram: 37,
+    Snapchat: 3,
+    Twitter: 1,
+    None: 37
+  }
+]
